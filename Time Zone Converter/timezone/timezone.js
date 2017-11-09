a09653f3313ea59d83aff7fd8891bb2d78465ac1
Neoh Teng Waey <tengwaey@hotmail.com>
2017-11-09 14:17:58 +0800

    Added CSS, icon, testing button
    Sorting file location

--- Time Zone Converter/timezone/timezone.js
+++ Time Zone Converter/timezone/timezone.js
@@ -3,3 +3,16 @@ function timeConvert(){
 
 	document.getElementById("output-time").value=document.getElementById("input-time").value;
 }
+
+var toggle = 0;
+
+function timeSwitch(){
+	if (toggle%2 ==0){
+		document.getElementById("switch-time").innerHTML="12 Hours Mode";
+		toggle++;
+	}
+	else{
+		document.getElementById("switch-time").innerHTML="24 Hours Mode";
+		toggle++;
+	}	
+}
